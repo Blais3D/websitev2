493338a43329feca100e1db2c4fd32e6af262b5c
Blais3D <LinwoodBlaisdell@gmail.com>
2022-03-20 18:16:15 -0400

    e

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,7 +10,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     </head>
     <body>
     <div className="bg-[#F5BE9A] w-screen h-[100px] space-y-10 text-center" >
-      <p className='font-extrabold text-4xl text-[#190e0e]'>Welcom to Blais.gg</p>
+      <p className='font-extrabold text-4xl text-[#190e0e]'>Welcome to Blais.gg</p>
       <img className="w-[100%]" src="/newWave.svg" alt="Nope"/>
     </div>
     </body>
