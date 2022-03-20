cf3bb04b377152a3315b4f18068009e955d58b1f
Blais3D <LinwoodBlaisdell@gmail.com>
2022-03-20 17:48:26 -0400

    cool wave and title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,26 +1,20 @@
 import type { AppProps } from 'next/app'
 import './index.css'
 
-const MyButton: React.FC<{ text: String; color: string; link: any }> = (
-  props
-) => {
-  return (
-    <button className='' onClick={props.link}>
-      {props.text}
-    </button>
-  );
-};
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-  <html>
-    <body className='bg-red-50'>
-      <h1 className='font-bold underline '>
-        Hello There
-      </h1>
+  <html className="bg-[#190e0e]">
+    <head>
+      <title>Welcome!</title>
+    </head>
+    <body className="bg-[#F5BE9A] w-screen h-[100px] flex place-content-center place-items-center">
+      <h1 className='font-extrabold text-4xl text-[#190e0e]'>Welcom to Blais.gg</h1>
+    </body>
+    <body className="w-screen place-content-end place-items-end">
+    <img className="w-[100%]" src="/newWave.svg" alt="Nope"/>
     </body>
   </html>)
-  //return <Component {...pageProps} />
 }
 
 export default MyApp
