82b696757557c551c56fbde555ce6730401e9a72
Blais3D <LinwoodBlaisdell@gmail.com>
2022-03-20 23:18:33 -0400

    Twitter hooked up

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,26 +1,25 @@
 import type { AppProps } from 'next/app'
 import './index.css'
-import {FaTwitterSquare, FaYoutubeSquare, FaGithubSquare } from 'react-icons/fa'
+import {FaTwitterSquare, FaYoutubeSquare, FaGithubSquare, FaDiscord } from 'react-icons/fa'
 import {MdMarkEmailUnread } from 'react-icons/md'
+import { TwitterTimelineEmbed, TwitterShareButton, TwitterFollowButton, TwitterHashtagButton, TwitterMentionButton, TwitterTweetEmbed, TwitterMomentShare, TwitterDMButton, TwitterVideoEmbed, TwitterOnAirButton } from 'react-twitter-embed';
 
-const MyButton: React.FC<{}> = (props) => {
+const MyButton: React.FC<{icon: any, link: string, size: number, text: string}> = (props) => {
   return (
-    <div className=' fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-gray-900 text-white shadow'>
-      <MyButtonIcon icon={<FaTwitterSquare size={28}/>}/>
-      <i>B</i>
-      <i>C</i>
-      <i>D</i>
-    </div>
+    <a className='text-[#F5BE9A] hover:text-gray-400 flex place-content-center place-items-center' href={props.link}>
+      <p className='font-extrabold text-3xl'>{props.text}</p>
+      <FaTwitterSquare size={props.size}/>
+    </a>
   );
 };
 
-const MyButtonIcon: React.FC<{icon: any}> = (props) => {
-  return (
-    <div className='sidebar-icon'>
-      {props.icon}
+const MyDiv: React.FC<{inside: any}> = (props) => {
+  return(
+    <div className='bg-[#311415] h-[700px] w-[600px] rounded-3xl p-5 text-center space-y-5'>
+      {props.inside}
     </div>
   );
-};
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -29,15 +28,31 @@ function MyApp({ Component, pageProps }: AppProps) {
       <title>Welcome!</title>
     </head>
     <body>
-    <div className="bg-[#F5BE9A] w-screen h-[100px] space-y-10 text-center" >
-      <div className='text-center'>
-        
-      <p className='font-extrabold text-4xl text-[#190e0e]'>Welcome to Blais.gg</p>
+    <div className="bg-[#F5BE9A] w-screen h-auto text-center" >
+      <div className='text-center place-content-center place-items-center p-10'>
+      <p className='font-extrabold text-5xl text-[#190e0e]'>Welcome to Blais.gg</p>
       </div>
       <img className="w-[100%]" src="/newWave.svg" alt="Nope"/>
     </div>
+    <div className='text-center'>
+      <p className='font-extrabold text-4xl text-[#F5BE9A]'>Socials</p>
+    </div>
+    <div className='flex place-content-center place-items-center'>
+      <div className='block p-10 space-y-10'>
+        <MyDiv inside={
+                  <>
+                  <MyButton icon={FaTwitterSquare} link={'https://twitter.com/LinwoodBlais'} size={45} text={"Twitter"} />
+                  <TwitterTimelineEmbed sourceType="profile" screenName="linwoodblais" options={{ height: 575, width: 450 }} />
+                  </>}>
+        </MyDiv>
+        <div className='bg-[#311415] h-[700px] w-[600px] rounded-3xl p-5'></div>
+      </div>
+      <div className='block p-10 space-y-10'>
+        <div className='bg-[#311415] h-[700px] w-[600px] rounded-3xl p-5'></div>
+        <div className='bg-[#311415] h-[700px] w-[600px] rounded-3xl p-5'></div>
+      </div>
+    </div>
     </body>
-    
   </html>)
 }
 
