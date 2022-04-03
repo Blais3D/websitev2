cae15afcdf7eceb3acd83a4885d225c1e1f52ec2
Blais3D <LinwoodBlaisdell@gmail.com>
2022-04-03 17:04:15 -0400

    fix?

--- next.config.js
+++ next.config.js
@@ -3,11 +3,4 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = {
-  webpack5: true,
-  webpack: (config) => {
-    config.resolve.fallback = { fs: false };
-
-    return config;
-  },
-};
+module.exports = nextConfig
